bc0b7b21ac73200710a36717ebcb1733ac291ece
Gary Bermudez and Nayomi Morita <gary+nayomi@>
2019-03-27 14:52:08 -0700

    Sending Data Down with Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Task } from './models/task.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
@@ -15,26 +14,18 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
   selectedTask = null;
 
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
+
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
   }
+
   finishedEditing() {
     this.selectedTask = null;
   }
 
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus courses', 3),
-    new Task('Begin brianstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on Github', 2)
-  ];
-
-  priorityColor(currentTask){
-      if (currentTask.priority === 3){
-        return "bg-danger";
-      } else if (currentTask.priority === 2) {
-        return  "bg-warning";
-      } else {
-        return "bg-info";
-      }
-    }
-  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule }  from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TaskListComponent,
+
   ],
   imports: [
     BrowserModule,
